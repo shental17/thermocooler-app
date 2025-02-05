448dea16a8ac8f743c0ef555498edbf09078e100
shental17 <133120470+shental17@users.noreply.github.com>
2025-02-05 18:11:24 +0800

    Update main.ino

--- firmware/thermocooler-app/main/main.ino
+++ firmware/thermocooler-app/main/main.ino
@@ -5,16 +5,16 @@
 #include <ESP_Google_Sheet_Client.h>
 #include <GS_SDHelper.h>
 
-#define WIFI_SSID "Shontol"
-#define WIFI_PASSWORD "ineedhelp"
+#define WIFI_SSID 
+#define WIFI_PASSWORD 
 // Google Project ID
-#define PROJECT_ID "thermocooler-datalogging"
+#define PROJECT_ID
 // Service Account's client email
-#define CLIENT_EMAIL "thermocooler-datalogging@thermocooler-datalogging.iam.gserviceaccount.com"
+#define CLIENT_EMAIL 
 // Service Account's private key
-const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDT92X7PMiXS0CH\nvdRo1TfN0d98r6HV+Cl+EX6UJtWKMp6zj5lh/RVXorIa3NMgFHOUDpvD7JeIYyBi\nCgQmOOCUCnOdXWQY0sA+9exIwEdruVI2W3uUEKS/IdTF2cNQzNbnCS8peGejhmgL\nIIFypZlt+HnSyk5rpvuG/T7U6gu5MifWs1FRJxTcxKCI3NF8zVGa5jNHmA3oXnW+\nXRrMR4Ht2ZqskwNh67aigz2yp/7VZQRVBDUE9DHpj6h016rN8vrOufpFQYjArcb1\nYHd3e0xZJgwHYQiVsksEfYLMdsTiO7vrb8NrKxFF8zyT8bgdBLGOwgsZZpmKpj0o\nUB7hdEXpAgMBAAECggEABJBIBs+5NeCt4/5HS3aDvA+vsxyI7HVqV0eDJSds44vR\ndOY7lCuBwSBEm2VzujYWgvKJkf9PB+WydcnnavdAho9vC81N8WbgMrrSuoJBR2V3\nCqqPj5GIKoFM9nGIzf2LBvBgrpDkElY7eTna75/3HLID1XD7nT+8HJzpNGZRmEwd\nFJN0iKOOl7lnOD5ZXpE/8EBVJs7xYDAyLTwgaA0eiwjHDpRft6gdWseZl5NNwfoq\njxFYWU372DRFOFDWcGvyj46pxGdW85JnnrgofmsrWhZoDu/aQM206LjWOlbsHf+e\nUDm+GVnvj2RXipZgTt1jb/D05tIFrSAPU5di0IoG8QKBgQD9UpLMRikUWyeUizbZ\nnL5m2FE7X1pyAg5tqiirzEQEeVqoDarMjUPYfMX6i85OEScAlWWs3Ea08f6BSWni\nIQC5D/cbXP9HRyOybrni7y2zY6sjOBjxVzFsTCugtaYUp6PX2+NpeHrUcicLG/sY\nBqcJxa76jYhe3iVFCJWTR7Bl1QKBgQDWNOz5JGBLv9himQgxZ3mwP4oCZ5aok3ng\nRGkdJGLQtE04C1M0d24ro0xYKn91YzCsQYquJENS10BWx6GJTxN2RMxtgBkBoHuH\n5Nqcxg0MqmlIuYamdMI8ziNDqtYMaRZ5Oc/I95StEvQey0Fk5Z2lXfVWnpHBodjh\nsJAytnXFxQKBgD7+TqTfeY2nUUNeDwi0MqDOf/y8rHgXTNE2dCCsjely5/lMVMHD\nCzEFbhHsvCOrnkBF5l/C8w5B8yDhSaRCwrRCZRd327800IqpgC4PfhTS3xbF92Xk\nYdTlaB/xq9gNC/A8rONoY1r6+/yGO71wwg7rCqUhltzIbwoopVFXjWT1AoGBAKvo\nxdYdL3sbeBmJH14m4S5lj+jmIa8gzEsEHCfbL8K3MNGtpRUSVESJRZfsY2ZeOI3F\n+/pN+Anf3i7HAAkmgUQFatrmVytLFFi3fgsblxV3Hg/TdRDZ25jZBswjqq1DGVoo\n32015QgRkgG0tcK31JEGmwLtN6TisyBNhyxr6pDRAoGBAKVjW9wOZXSp8r4IpFM5\nkLifwOYs5YFbkcpM4U1GjPuCwKOpphN0qy/ZdphcSxEkzPugnpejciwqAHs4WdnY\nQAQibOoafSRKU4lDIxnm0ytDAhL7SFcDUxSPsrXO8wDjgkZXdpqVH1A4WL350jhk\naMCntPP7oT2WL6EBJOr03zKy\n-----END PRIVATE KEY-----\n";
+const char PRIVATE_KEY[] PROGMEM 
 // The ID of the spreadsheet where you'll publish the data
-const char spreadsheetId[] = "14JQdsmQZBLRAVksC1eRPhfUaqvE9ZvPd0U7fP0uj8Qg";
+const char spreadsheetId[] = 
 
 // Timer variables
 unsigned long lastTime = 0;  
